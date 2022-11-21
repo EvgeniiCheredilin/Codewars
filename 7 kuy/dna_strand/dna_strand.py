3141cd35df7192e779ab0c81ad3b5312198a8fed
EvgeniiCheredilin <evgenii.cheredilin@gmail.com>
2022-11-21 10:16:28 +0300

    Add dna_strand function

---
+++ 7 kuy/dna_strand/dna_strand.py
@@ -0,0 +1,12 @@
+def dna_strand(dna):
+    my_dna = ""
+    for i in dna:
+        if i == "A":
+            my_dna = my_dna + "T"
+        if i == "T":
+            my_dna = my_dna + "A"
+        if i == "C":
+            my_dna = my_dna + "G"
+        if i == "G":
+            my_dna = my_dna + "C"
+    return my_dna
